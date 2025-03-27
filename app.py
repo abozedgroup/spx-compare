777510d57ea9f33fc9e3ae15d0ab6cfb0a7866dd
abozedgroup <151201147+abozedgroup@users.noreply.github.com>
2025-03-27 07:43:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(layout="wide")
-st.title("تحقق من بيانات Google Sheets")
+st.title("فحص بيانات Google Sheets")
 
 @st.cache_data(ttl=86400)
 def load_raw_data():
@@ -11,8 +11,8 @@ def load_raw_data():
 
 df = load_raw_data()
 
-st.write("أسماء الأعمدة:")
+st.subheader("أسماء الأعمدة:")
 st.write(df.columns.tolist())
 
-st.write("أول 20 صف:")
+st.subheader("أول 20 صف من البيانات:")
 st.dataframe(df.head(20))
